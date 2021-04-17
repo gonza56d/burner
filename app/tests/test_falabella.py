16b23a48147b2dae63fc6c5d203d4e7b2281c173
gonza <gonza56d@gmail.com>
2021-04-16 23:35:13 -0300

    FalabellaPage test get categories

--- app/tests/test_falabella.py
+++ app/tests/test_falabella.py
@@ -11,3 +11,10 @@ class TestFalabellaPage(unittest.TestCase):
 
     def setUp(self) -> None:
         self.page = FalabellaPage()
+
+    def test_get_categories(self) -> None:
+        categories = self.page.get_categories()
+        self.assertGreater(len(categories), 0, "No categories where found.")
+        for category in categories:
+            self.assertIsNotNone(category.name, "Category didn't have a name")
+            self.assertIsNotNone(category.href, "Category didn't have an href")
