08ea56b41f2edba0a94935edfef1c8517c618e6b
gonza <gonza56d@gmail.com>
2021-04-17 00:12:38 -0300

    FalabellaPage test get sales

--- app/tests/test_falabella.py
+++ app/tests/test_falabella.py
@@ -14,7 +14,20 @@ class TestFalabellaPage(unittest.TestCase):
 
     def test_get_categories(self) -> None:
         categories = self.page.get_categories()
-        self.assertGreater(len(categories), 0, "No categories where found.")
+        self.assertGreater(len(categories), 0, "No categories where found")
         for category in categories:
             self.assertIsNotNone(category.name, "Category didn't have a name")
             self.assertIsNotNone(category.href, "Category didn't have an href")
+
+    def test_get_sales(self) -> None:
+        sales = self.page.get_sales()
+        self.assertGreater(len(sales), 0, "No sales where found")
+        for sale in sales:
+            self.assertIsNotNone(
+                sale.product_id,
+                "Sale didn't have an ID"
+            )
+            self.assertIsNotNone(
+                sale.product_name,
+                "Sale didn't have a product name"
+            )
