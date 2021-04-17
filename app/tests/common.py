699e83a10deb67dc63b51973b270eb3e7b701116
gonza <gonza56d@gmail.com>
2021-04-17 00:51:14 -0300

    CommonTests base class

--- app/tests/common.py
+++ app/tests/common.py
@@ -1,28 +1,30 @@
 # Python
-import unittest
-# App
-from app.pages import FalabellaPage
+from typing import List
 
 
-class TestFalabellaPage(unittest.TestCase):
+class CommonTests:
     """
-    Falabella web page unit tests.
+    Common web page unit tests.
     """
-
-    def setUp(self) -> None:
-        self.page = FalabellaPage()
 
     def test_get_categories(self) -> None:
-        categories = self.page.get_categories()
-        self.assertGreater(len(categories), 0, "No categories where found")
-        for category in categories:
+        page_categories = self.page.get_categories()
+        self.assertGreater(len(page_categories), 0, "No categories where found")
+        for category in page_categories:
             self.assertIsNotNone(category.id, "Category didn't have an id")
             self.assertIsNotNone(category.name, "Category didn't have a name")
             self.assertIsNotNone(category.href, "Category didn't have an href")
 
     def test_get_sales(self) -> None:
-        sales = self.page.get_sales()
-        self.assertGreater(len(sales), 0, "No sales where found")
+        page_sales = self.page.get_sales()
+        self.assertGreater(len(page_sales), 0, "No sales where found")
+        self.validate_sales(page_sales)
+
+    def test_csv_results(self) -> None:
+        csv_results = self.page.get_result_csv()  # TODO must return a list of objects
+        self.validate_sales(csv_results)
+
+    def validate_sales(self, sales: List) -> None:
         for sale in sales:
             self.assertIsNotNone(
                 sale.product_id,
