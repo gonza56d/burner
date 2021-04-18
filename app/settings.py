749d0e7343b5bcbee0bfcc884f91c849378d171d
gonza <gonza56d@gmail.com>
2021-04-18 10:33:46 -0300

    settings fix

--- app/settings.py
+++ app/settings.py
@@ -4,4 +4,4 @@ Project variable settings.
 
 # Selenium webdriver path
 # Set the indicated driver for the execution environment
-SELENIUM_DRIVER_PATH = '.app/linux_chromedriver'
+SELENIUM_DRIVER_PATH = './app/linux_chromedriver'
