b6cd9f29d88d648f5fb9bfcad2d15197aa10809a
gonza <gonza56d@gmail.com>
2021-04-24 08:01:37 -0300

    base selenium utils docstring improvements

--- app/pages/base/selenium_utils.py
+++ app/pages/base/selenium_utils.py
@@ -9,12 +9,27 @@ from settings import SELENIUM_DRIVER_PATH
 
 
 class BaseSeleniumUtils(ABC):
+    """Base abstract class for selenium classes.
+
+    Implement Chromedriver and get to page url.
+    """
 
     def __init__(self) -> None:
+        """Constructor method.
+
+        Set Chromedriver instance, maximize window and get to the page url
+        to get ready for scraping.
+        """
         self.driver = Chrome(executable_path=SELENIUM_DRIVER_PATH)
         self.driver.maximize_window()
         self.driver.get(self.get_page_url())
 
     @abstractmethod
-    def get_page_url(self):
+    def get_page_url(self) -> str:
+        """Get class attribute of page url.
+
+        Return
+        ------
+        str : Page url.
+        """
         pass
