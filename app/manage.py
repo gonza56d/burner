35c09475fd509fbc49408cd15797a4d99dcef086
gonza <gonza56d@gmail.com>
2021-04-24 04:34:18 -0300

    manage.py dosctrings improvements

--- app/manage.py
+++ app/manage.py
@@ -1,6 +1,4 @@
-"""
-Handle scripts with execution command lines.
-"""
+"""Handle scripts with execution command lines."""
 
 # Python
 from enum import Enum
@@ -13,32 +11,37 @@ from utils.exceptions import CommandExecutionException
 
 
 class SubCommand:
-    """
-    Handle subcommands logic.
+    """Handle subcommands logic.
     """
 
     USAGE = " * Usage: manage.py --pages='$PAGE_1 $PAGE_2 $PAGE_N' --tasks='$TASK_1 $TASK_2 $TASK_N'"
     HELP_COMMAND = '-help'
 
     class Pages(Enum):
-        """
-        Pages options to execute tasks from.
+        """Enum with pages options to execute tasks from.
         """
         FALABELLA = 'falabella'
         SODIMAC = 'sodimac'
 
     class Tasks(Enum):
-        """
-        Available tasks to execute from any page. 
+        """Enum with available tasks to execute from any page. 
         """
         COLLECTCATEGORIES = 'collectcategories'
         COLLECTPRODUCTS = 'collectproducts'
 
     @staticmethod
     def tasks_to_page_methods(tasks: List[str]) -> List[str]:
-        """
-        Convert tasks passed as subcommand into the corresponding
+        """Convert tasks passed as subcommand into the corresponding
         page method name.
+
+        Parameters
+        ----------
+        tasks : List[str]
+            Tasks to convert to the corresponding page method expression.
+        
+        Return
+        ------
+        List[str] : Collection of method expressions.
         """
         page_methods = []
         for task in tasks:
@@ -50,28 +53,51 @@ class SubCommand:
 
     @staticmethod
     def pages_to_page_objects(pages: List[str]) -> List[str]:
-        """
-        Convert pages passed as subcommand into the corresponding
+        """Convert pages passed as subcommand into the corresponding
         page object name.
+
+        Parameters
+        ----------
+        pages : List[str]
+            Pages to convert to the corresponding page class expression.
+
+        Return
+        ------
+        List[str] : Collection of page class expressions.
         """
         return [page.title() + 'Page' for page in pages]
 
     def __init__(self, argv: List[str]) -> None:
+        """Constructor.
+
+        Parameters
+        ----------
+        argv : List[str]
+            Command line arguments.
+        """
         self.called_help = SubCommand.check_if_called_help(argv)
         if self.called_help:
             return
         self.pages = None
         self.tasks = None
         arguments = self.validate_arguments(argv)
-        self.get_pages_and_tasks(arguments)
+        self.set_pages_and_tasks(arguments)
         if self.pages is None or self.tasks is None:
             self.throw_common_exception()
         self.validate_data()
 
     def validate_arguments(self, argv: List[str]) -> str:
-        """
-        Validate that all the parts of the command are present and return them
-        in a single string to handle later in get_pages_and_tasks().
+        """Validate that all the parts of the command are present and return
+        them concatenated in a single string to handle later in set_pages_and_tasks().
+
+        Parameters
+        ----------
+        argv : List[str]
+            Command line arguments.
+        
+        Return
+        ------
+        str : Concatenated argv values.
         """
         try:
             first_arg = argv[1]
@@ -81,8 +107,7 @@ class SubCommand:
         return first_arg + second_arg
 
     def validate_data(self) -> None:
-        """
-        Validate that pages and tasks sent as subcommands are valid.
+        """Validate that pages and tasks sent as subcommands are valid.
         """
         for page in self.pages:
             if page.upper() not in SubCommand.Pages.__members__:
@@ -98,15 +123,21 @@ class SubCommand:
                 )
 
     def throw_common_exception(self):
+        """Raise a common exception when command was sent with an error.
+        """
         raise CommandExecutionException(
             f"\n{self.USAGE}\n"
             f" * Heads up: Each page is executed asynchronously, running their sent tasks in order.\n"
             f" * See manage.py {self.HELP_COMMAND}"
         )
 
-    def get_pages_and_tasks(self, arguments: str) -> None:
-        """
-        Handle commandline to compose the pages and tasks intended to run.
+    def set_pages_and_tasks(self, arguments: str) -> None:
+        """Handle arguments to compose the pages and tasks intended to run.
+
+        Parameters
+        ----------
+        arguments : str
+            Options sent in the command line.
         """
         splitted = arguments.split('--')
         for val in splitted:
@@ -117,8 +148,16 @@ class SubCommand:
 
     @classmethod
     def check_if_called_help(cls, argv: List[str]) -> bool:
-        """
-        Check if user called help command and print instructions if true.
+        """Check if user called help command and print instructions if true.
+
+        Parameters
+        ----------
+        argv : List[str]
+            Options sent in the command line to check if help was called.
+
+        Return
+        ------
+        bool : True if help command has been sent.
         """
         try:
             if argv[1] == cls.HELP_COMMAND:
@@ -142,8 +181,15 @@ class SubCommand:
 
 
 def run(pages: List[str], methods: List[str]) -> None:
-    """
-    Execute pages with their own tasks in the order sent by user.
+    """Execute pages with their own tasks in the order sent by user.
+
+    Parameters
+    ----------
+    pages : List[str]
+        Expressions of page classes to execute tasks from.
+
+    methods : List[str]
+        Expressions of page methods to run.
     """
     for method in methods:
         for page in pages:
@@ -151,8 +197,7 @@ def run(pages: List[str], methods: List[str]) -> None:
 
 
 def main() -> None:
-    """
-    Handle how to execute tasks from command line.
+    """Handle how to execute tasks from command line.
     """
 
     sub_command = SubCommand(sys.argv)
